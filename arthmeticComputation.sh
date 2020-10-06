a4f8588959372435d916714a67330f1e17e7a5ab
poojakakuru <poojareddykakuru@gmail.com>
2020-10-06 17:05:31 +0530

    computation1 is completed

--- arthmeticComputation.sh
+++ arthmeticComputation.sh
@@ -5,3 +5,6 @@ echo "Welcome to arthmetic operations"
 read -p "Enter number 1:" a
 read -p "Enter number 2:" b
 read -p "Enter number 3:" c
+
+computation1=$(( $a+$b*$c ))
+echo "first computation is:" $computation1

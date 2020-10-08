594b0811cfb16d6d9cb7ddb6c9c61840ff3f16db
poojakakuru <poojareddykakuru@gmail.com>
2020-10-08 09:17:42 +0530

    storing computations in dictionaries

--- arthmeticComputation.sh
+++ arthmeticComputation.sh
@@ -17,3 +17,11 @@ echo "third computation is:"$computation3
 
 computation4=$(( $a%$b+$c ))
 echo "Fourth Computation is:"$computation4
+
+declare -A computations
+computation[1]=$computation1
+computation[2]=$computation2
+computation[3]=$computation3
+computation[4]=$computation4
+
+echo "dictionary values are :" ${computation[@]}
